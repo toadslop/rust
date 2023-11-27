222786bfdd5a9f5345ab33aeb42cde02df56a6c8
Remo Senekowitsch <remo@buenzli.dev>
2023-11-27 15:31:07 +0100

    wip

--- exercises/practice/forth/tests/forth.rs
+++ exercises/practice/forth/tests/forth.rs
@@ -1,373 +1,439 @@
-use forth::{Error, Forth, Value};
-
 #[test]
-fn no_input_no_stack() {
-    assert_eq!(Vec::<Value>::new(), Forth::new().stack());
+fn numbers_just_get_pushed_onto_the_stack() {
+    let input = {"instructions":["1 2 3 4 5"]};
+    let output = forth::new(input);
+    let expected = [1,2,3,4,5];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn numbers_just_get_pushed_onto_the_stack() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 3 4 5").is_ok());
-    assert_eq!(vec![1, 2, 3, 4, 5], f.stack());
+fn pushes_negative_numbers_onto_the_stack() {
+    let input = {"instructions":["-1 -2 -3 -4 -5"]};
+    let output = forth::new(input);
+    let expected = [-1,-2,-3,-4,-5];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn can_add_two_numbers() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 +").is_ok());
-    assert_eq!(vec![3], f.stack());
+    let input = {"instructions":["1 2 +"]};
+    let output = forth::new(input);
+    let expected = [3];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn addition_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("1 +"));
-    assert_eq!(Err(Error::StackUnderflow), f.eval("+"));
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["+"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn errors_if_there_is_only_one_value_on_the_stack() {
+    let input = {"instructions":["1 +"]};
+    let output = forth::new(input);
+    let expected = {"error":"only one value on the stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn can_subtract_two_numbers() {
-    let mut f = Forth::new();
-    assert!(f.eval("3 4 -").is_ok());
-    assert_eq!(vec![-1], f.stack());
+    let input = {"instructions":["3 4 -"]};
+    let output = forth::new(input);
+    let expected = [-1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn subtraction_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("1 -"));
-    assert_eq!(Err(Error::StackUnderflow), f.eval("-"));
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["-"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn errors_if_there_is_only_one_value_on_the_stack() {
+    let input = {"instructions":["1 -"]};
+    let output = forth::new(input);
+    let expected = {"error":"only one value on the stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn can_multiply_two_numbers() {
-    let mut f = Forth::new();
-    assert!(f.eval("2 4 *").is_ok());
-    assert_eq!(vec![8], f.stack());
+    let input = {"instructions":["2 4 *"]};
+    let output = forth::new(input);
+    let expected = [8];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn multiplication_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("1 *"));
-    assert_eq!(Err(Error::StackUnderflow), f.eval("*"));
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["*"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn errors_if_there_is_only_one_value_on_the_stack() {
+    let input = {"instructions":["1 *"]};
+    let output = forth::new(input);
+    let expected = {"error":"only one value on the stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn can_divide_two_numbers() {
-    let mut f = Forth::new();
-    assert!(f.eval("12 3 /").is_ok());
-    assert_eq!(vec![4], f.stack());
+    let input = {"instructions":["12 3 /"]};
+    let output = forth::new(input);
+    let expected = [4];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn performs_integer_division() {
-    let mut f = Forth::new();
-    assert!(f.eval("8 3 /").is_ok());
-    assert_eq!(vec![2], f.stack());
-}
-
-#[test]
-#[ignore]
-fn division_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("1 /"));
-    assert_eq!(Err(Error::StackUnderflow), f.eval("/"));
+    let input = {"instructions":["8 3 /"]};
+    let output = forth::new(input);
+    let expected = [2];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn errors_if_dividing_by_zero() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::DivisionByZero), f.eval("4 0 /"));
+    let input = {"instructions":["4 0 /"]};
+    let output = forth::new(input);
+    let expected = {"error":"divide by zero"};
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["/"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn errors_if_there_is_only_one_value_on_the_stack() {
+    let input = {"instructions":["1 /"]};
+    let output = forth::new(input);
+    let expected = {"error":"only one value on the stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn addition_and_subtraction() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 + 4 -").is_ok());
-    assert_eq!(vec![-1], f.stack());
+    let input = {"instructions":["1 2 + 4 -"]};
+    let output = forth::new(input);
+    let expected = [-1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn multiplication_and_division() {
-    let mut f = Forth::new();
-    assert!(f.eval("2 4 * 3 /").is_ok());
-    assert_eq!(vec![2], f.stack());
+    let input = {"instructions":["2 4 * 3 /"]};
+    let output = forth::new(input);
+    let expected = [2];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn dup() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 dup").is_ok());
-    assert_eq!(vec![1, 1], f.stack());
+fn copies_a_value_on_the_stack() {
+    let input = {"instructions":["1 dup"]};
+    let output = forth::new(input);
+    let expected = [1,1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn dup_top_value_only() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 dup").is_ok());
-    assert_eq!(vec![1, 2, 2], f.stack());
+fn copies_the_top_value_on_the_stack() {
+    let input = {"instructions":["1 2 dup"]};
+    let output = forth::new(input);
+    let expected = [1,2,2];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn dup_case_insensitive() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 DUP Dup dup").is_ok());
-    assert_eq!(vec![1, 1, 1, 1], f.stack());
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["dup"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn dup_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("dup"));
+fn removes_the_top_value_on_the_stack_if_it_is_the_only_one() {
+    let input = {"instructions":["1 drop"]};
+    let output = forth::new(input);
+    let expected = [];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn drop() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 drop").is_ok());
-    assert_eq!(Vec::<Value>::new(), f.stack());
+fn removes_the_top_value_on_the_stack_if_it_is_not_the_only_one() {
+    let input = {"instructions":["1 2 drop"]};
+    let output = forth::new(input);
+    let expected = [1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn drop_with_two() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 drop").is_ok());
-    assert_eq!(vec![1], f.stack());
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["drop"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn drop_case_insensitive() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 3 4 DROP Drop drop").is_ok());
-    assert_eq!(vec![1], f.stack());
+fn swaps_the_top_two_values_on_the_stack_if_they_are_the_only_ones() {
+    let input = {"instructions":["1 2 swap"]};
+    let output = forth::new(input);
+    let expected = [2,1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn drop_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("drop"));
+fn swaps_the_top_two_values_on_the_stack_if_they_are_not_the_only_ones() {
+    let input = {"instructions":["1 2 3 swap"]};
+    let output = forth::new(input);
+    let expected = [1,3,2];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn swap() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 swap").is_ok());
-    assert_eq!(vec![2, 1], f.stack());
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["swap"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn swap_with_three() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 3 swap").is_ok());
-    assert_eq!(vec![1, 3, 2], f.stack());
+fn errors_if_there_is_only_one_value_on_the_stack() {
+    let input = {"instructions":["1 swap"]};
+    let output = forth::new(input);
+    let expected = {"error":"only one value on the stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn swap_case_insensitive() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 SWAP 3 Swap 4 swap").is_ok());
-    assert_eq!(vec![2, 3, 4, 1], f.stack());
+fn copies_the_second_element_if_there_are_only_two() {
+    let input = {"instructions":["1 2 over"]};
+    let output = forth::new(input);
+    let expected = [1,2,1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn swap_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("1 swap"));
-    assert_eq!(Err(Error::StackUnderflow), f.eval("swap"));
+fn copies_the_second_element_if_there_are_more_than_two() {
+    let input = {"instructions":["1 2 3 over"]};
+    let output = forth::new(input);
+    let expected = [1,2,3,2];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn over() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 over").is_ok());
-    assert_eq!(vec![1, 2, 1], f.stack());
+fn errors_if_there_is_nothing_on_the_stack() {
+    let input = {"instructions":["over"]};
+    let output = forth::new(input);
+    let expected = {"error":"empty stack"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn over_with_three() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 3 over").is_ok());
-    assert_eq!(vec![1, 2, 3, 2], f.stack());
+fn errors_if_there_is_only_one_value_on_the_stack() {
+    let input = {"instructions":["1 over"]};
+    let output = forth::new(input);
+    let expected = {"error":"only one value on the stack"};
+    assert_eq!(output, expected);
 }
-
-#[test]
-#[ignore]
-fn over_case_insensitive() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 OVER Over over").is_ok());
-    assert_eq!(vec![1, 2, 1, 2, 1], f.stack());
-}
-
-#[test]
-#[ignore]
-fn over_error() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::StackUnderflow), f.eval("1 over"));
-    assert_eq!(Err(Error::StackUnderflow), f.eval("over"));
-}
-
-// User-defined words
 
 #[test]
 #[ignore]
 fn can_consist_of_built_in_words() {
-    let mut f = Forth::new();
-    assert!(f.eval(": dup-twice dup dup ;").is_ok());
-    assert!(f.eval("1 dup-twice").is_ok());
-    assert_eq!(vec![1, 1, 1], f.stack());
+    let input = {"instructions":[": dup-twice dup dup ;","1 dup-twice"]};
+    let output = forth::new(input);
+    let expected = [1,1,1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn execute_in_the_right_order() {
-    let mut f = Forth::new();
-    assert!(f.eval(": countup 1 2 3 ;").is_ok());
-    assert!(f.eval("countup").is_ok());
-    assert_eq!(vec![1, 2, 3], f.stack());
+    let input = {"instructions":[": countup 1 2 3 ;","countup"]};
+    let output = forth::new(input);
+    let expected = [1,2,3];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn redefining_an_existing_word() {
-    let mut f = Forth::new();
-    assert!(f.eval(": foo dup ;").is_ok());
-    assert!(f.eval(": foo dup dup ;").is_ok());
-    assert!(f.eval("1 foo").is_ok());
-    assert_eq!(vec![1, 1, 1], f.stack());
+fn can_override_other_user_defined_words() {
+    let input = {"instructions":[": foo dup ;",": foo dup dup ;","1 foo"]};
+    let output = forth::new(input);
+    let expected = [1,1,1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn redefining_an_existing_built_in_word() {
-    let mut f = Forth::new();
-    assert!(f.eval(": swap dup ;").is_ok());
-    assert!(f.eval("1 swap").is_ok());
-    assert_eq!(vec![1, 1], f.stack());
+fn can_override_built_in_words() {
+    let input = {"instructions":[": swap dup ;","1 swap"]};
+    let output = forth::new(input);
+    let expected = [1,1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn user_defined_words_are_case_insensitive() {
-    let mut f = Forth::new();
-    assert!(f.eval(": foo dup ;").is_ok());
-    assert!(f.eval("1 FOO Foo foo").is_ok());
-    assert_eq!(vec![1, 1, 1, 1], f.stack());
-}
-
-#[test]
-#[ignore]
-fn definitions_are_case_insensitive() {
-    let mut f = Forth::new();
-    assert!(f.eval(": SWAP DUP Dup dup ;").is_ok());
-    assert!(f.eval("1 swap").is_ok());
-    assert_eq!(vec![1, 1, 1, 1], f.stack());
-}
-
-#[test]
-#[ignore]
-fn redefining_a_built_in_operator() {
-    let mut f = Forth::new();
-    assert!(f.eval(": + * ;").is_ok());
-    assert!(f.eval("3 4 +").is_ok());
-    assert_eq!(vec![12], f.stack());
+fn can_override_built_in_operators() {
+    let input = {"instructions":[": + * ;","3 4 +"]};
+    let output = forth::new(input);
+    let expected = [12];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn can_use_different_words_with_the_same_name() {
-    let mut f = Forth::new();
-    assert!(f.eval(": foo 5 ;").is_ok());
-    assert!(f.eval(": bar foo ;").is_ok());
-    assert!(f.eval(": foo 6 ;").is_ok());
-    assert!(f.eval("bar foo").is_ok());
-    assert_eq!(vec![5, 6], f.stack());
+    let input = {"instructions":[": foo 5 ;",": bar foo ;",": foo 6 ;","bar foo"]};
+    let output = forth::new(input);
+    let expected = [5,6];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn can_define_word_that_uses_word_with_the_same_name() {
-    let mut f = Forth::new();
-    assert!(f.eval(": foo 10 ;").is_ok());
-    assert!(f.eval(": foo foo 1 + ;").is_ok());
-    assert!(f.eval("foo").is_ok());
-    assert_eq!(vec![11], f.stack());
+    let input = {"instructions":[": foo 10 ;",": foo foo 1 + ;","foo"]};
+    let output = forth::new(input);
+    let expected = [11];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn defining_a_number() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::InvalidWord), f.eval(": 1 2 ;"));
+fn cannot_redefine_non_negative_numbers() {
+    let input = {"instructions":[": 1 2 ;"]};
+    let output = forth::new(input);
+    let expected = {"error":"illegal operation"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn malformed_word_definition() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::InvalidWord), f.eval(":"));
-    assert_eq!(Err(Error::InvalidWord), f.eval(": foo"));
-    assert_eq!(Err(Error::InvalidWord), f.eval(": foo 1"));
+fn cannot_redefine_negative_numbers() {
+    let input = {"instructions":[": -1 2 ;"]};
+    let output = forth::new(input);
+    let expected = {"error":"illegal operation"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn calling_non_existing_word() {
-    let mut f = Forth::new();
-    assert_eq!(Err(Error::UnknownWord), f.eval("1 foo"));
+fn errors_if_executing_a_non_existent_word() {
+    let input = {"instructions":["foo"]};
+    let output = forth::new(input);
+    let expected = {"error":"undefined operation"};
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn multiple_definitions() {
-    let mut f = Forth::new();
-    assert!(f.eval(": one 1 ; : two 2 ; one two +").is_ok());
-    assert_eq!(vec![3], f.stack());
+fn only_defines_locally() {
+    let input = {"instructionsFirst":[": + - ;","1 1 +"],"instructionsSecond":["1 1 +"]};
+    let output = forth::new(input);
+    let expected = [[0],[2]];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn definitions_after_ops() {
-    let mut f = Forth::new();
-    assert!(f.eval("1 2 + : addone 1 + ; addone").is_ok());
-    assert_eq!(vec![4], f.stack());
+fn dup_is_case_insensitive() {
+    let input = {"instructions":["1 DUP Dup dup"]};
+    let output = forth::new(input);
+    let expected = [1,1,1,1];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn redefine_an_existing_word_with_another_existing_word() {
-    let mut f = Forth::new();
-    assert!(f.eval(": foo 5 ;").is_ok());
-    assert!(f.eval(": bar foo ;").is_ok());
-    assert!(f.eval(": foo 6 ;").is_ok());
-    assert!(f.eval(": bar foo ;").is_ok());
-    assert!(f.eval("bar foo").is_ok());
-    assert_eq!(vec![6, 6], f.stack());
+fn drop_is_case_insensitive() {
+    let input = {"instructions":["1 2 3 4 DROP Drop drop"]};
+    let output = forth::new(input);
+    let expected = [1];
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn swap_is_case_insensitive() {
+    let input = {"instructions":["1 2 SWAP 3 Swap 4 swap"]};
+    let output = forth::new(input);
+    let expected = [2,3,4,1];
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn over_is_case_insensitive() {
+    let input = {"instructions":["1 2 OVER Over over"]};
+    let output = forth::new(input);
+    let expected = [1,2,1,2,1];
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn user_defined_words_are_case_insensitive() {
+    let input = {"instructions":[": foo dup ;","1 FOO Foo foo"]};
+    let output = forth::new(input);
+    let expected = [1,1,1,1];
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn definitions_are_case_insensitive() {
+    let input = {"instructions":[": SWAP DUP Dup dup ;","1 swap"]};
+    let output = forth::new(input);
+    let expected = [1,1,1,1];
+    assert_eq!(output, expected);
 }
